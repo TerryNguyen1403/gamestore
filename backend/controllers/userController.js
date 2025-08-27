354d5be004aeb53286437eb8965819e3ea589408
TerryNguyen1403 <giangnht1403@gmail.com>
2025-08-27 11:08:09 +0700

    Hashing password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,20 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 require('dotenv').config();
 
 // Register user
 const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({
+            message: 'Vui lòng điền đầy đủ thông tin'
+        });
+    };
+
+    // Kiểm tra email đã tồn tại chưa
     let check = await User.findOne({
         email: req.body.email
     });
@@ -15,44 +25,49 @@ const registerUser = async (req, res) => {
         });
     }
 
-    let cart = [];
-
-    for (let i = 0; i < 300; i++) {
-        cart[i] = 0;
-    }
+    // Hash password trước khi lưu vào DB
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        cartData: cart
+        name: name,
+        email: email,        
+        password: hashedPassword
     });
 
-    await user.save();
+    try {
+        await user.save();
 
-    const data = {
-        user: {
-            id: user.id
+        const data = {
+            user: {
+                id: user.id
+            }
         }
-    }
 
-    const token = jwt.sign(data, process.env.JWT_SECRET);
-    res.status(200).json({ token });
+        const token = jwt.sign(data, process.env.JWT_SECRET);
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500);
+        throw new Error('Error: ', error);
+    }
 };
 
 // Login user
 const loginUser = async (req, res) => {
+    const { password } = req.body;
+
+    //  Kiểm tra mail
     let user = await User.findOne({
         email: req.body.email
     });
 
     if(!user){
         return res.status(400).json({
-            message: 'Email không tồn tại'
+            message: 'Người dùng không tồn tại'
         });
     }else {
         // Check password
-        const isMatch = req.body.password === user.password;
+        const isMatch = bcrypt.compare(password, user.password);
         if(!isMatch){
             return res.status(400).json({
                 message: 'Mật khẩu không chính xác'
